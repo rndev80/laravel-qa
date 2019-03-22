075a4fcc39495b334d202469e5485c971cd784bf
rndev80 <rndev80@gmail.com>
2019-03-23 00:22:21 +0100

    Add author info and question creation date in quetion item

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">All questions</div>
 
                 <div class="card-body">
                     @foreach ($questions as $question)
