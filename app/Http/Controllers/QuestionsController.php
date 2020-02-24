c40ac3de363ae3e379d4b145f71d4b660159a998
mlad9304 <mlad9304@gmail.com>
2020-02-24 15:57:58 +0300

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -66,9 +69,8 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if (\Gate::denies('update-question', $question)) {
-            abort(403, 'Access denied');
-        }
+        $this->authorize('update', $question);
+        
         return view('questions.edit', compact('question'));
     }
 
@@ -81,6 +83,8 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
+
         $question->update($request->only('title', 'body'));
 
         return redirect('/questions')->with('success', 'Your question has been updated.');
