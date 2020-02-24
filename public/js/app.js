6d0ec786fd292eb8724995dfc8b412e0d14c1be8
mlad9304 <mlad9304@gmail.com>
2020-02-24 06:00:44 +0300

    Implemented delete functionality for the question

--- public/js/app.js
+++ public/js/app.js
@@ -49180,8 +49180,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /media/hello/work/laravel-qa/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /media/hello/work/laravel-qa/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /media/hello/project/laravel/laravel-qa/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /media/hello/project/laravel/laravel-qa/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
